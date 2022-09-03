377bc44b11ce70ca696f0d0ceeda4b23c73f6cb9
Adrigondo <theadrigondo24@gmail.com>
2022-09-02 20:46:17 -0600

    Update: header navigation buttons

--- src/app/components/main/header/header.component.ts
+++ src/app/components/main/header/header.component.ts
@@ -7,9 +7,16 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Input('userLoged') userLoged:boolean=false; 
+
+  isFirstActive:boolean=true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  navigate(){
+    this.isFirstActive=!this.isFirstActive;
+    console.log(this.isFirstActive)
+  }
 }
