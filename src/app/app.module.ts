6ecc21da2af022345abab6801112c3334344d29d
pineda24 <85385878+pineda24@users.noreply.github.com>
2022-09-05 06:45:13 -0600

    graph

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { VotationComponent } from './components/votation/votation.component';
 // PRIMENG MODULES
 import {CheckboxModule} from 'primeng/checkbox';
 import { OptionCardComponent } from './molecules/option-card/option-card.component';
+import {ChartModule} from 'primeng/chart';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { OptionCardComponent } from './molecules/option-card/option-card.compone
     AppRoutingModule,
     FormsModule,
 
-    CheckboxModule
+    CheckboxModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/votation/votation.component.ts
+++ src/app/components/votation/votation.component.ts
@@ -1,39 +1,98 @@
-import { Component, ElementRef, Input, OnInit, ViewChildren } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChildren,
+} from '@angular/core';
 import { Votation, VotationOption } from 'src/app/models/votation.model';
 
 @Component({
   selector: 'app-votation',
   templateUrl: './votation.component.html',
-  styleUrls: ['./votation.component.css']
+  styleUrls: ['./votation.component.css'],
 })
 export class VotationComponent implements OnInit {
-  @Input("votationObject") votationObject:Votation=new Votation();
-  selectedOptions!:VotationOption[];
-  @ViewChildren("optionDescription") optionDescriptions!:HTMLDivElement[];
-  limitReached:boolean=false;
+  @Input('votationObject') votationObject: Votation = new Votation();
+  selectedOptions!: VotationOption[];
+  @ViewChildren('optionDescription') optionDescriptions!: HTMLDivElement[];
+  limitReached: boolean = false;
 
-  constructor() { }
+  data: any;
+  chartOptions: any;
+  basicData: any;
+  basicOptions: any;
+  pieOptions: any;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.data = {
+      labels: ['A', 'B'],
+      datasets: [
+        {
+          data: [300, 50],
+          backgroundColor: ['#9CC4F3', '#E77CBC'],
+          hoverBackgroundColor: ['#9CC4F3', '#E77CBC'],
+        },
+      ],
+    };
+
+    this.basicData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: '#9CC4F3',
+          tension: 0.4,
+        },
+        {
+          label: 'Second Dataset',
+          data: [28, 48, 40, 19, 86, 27, 90],
+          fill: false,
+          borderColor: '#E77CBC',
+          tension: 0.4,
+        },
+      ],
+    };
+
+    this.pieOptions = {
+      tooltips: {
+          callbacks: {
+              label: function (t:any, d:any) { // https://stackoverflow.com/a/44971825/10438278
+                  let v = d.datasets[0].data[t.index];
+                  var xLabel = d.labels[t.index];
+                  var yLabel = v >= 1000 ? v.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : v;
+                  return xLabel + ': ' + yLabel;
+              }
+          }
+      },
+      legend: {
+          position: 'right'
+      }
+  };
   }
 
-  toogleDisplayDescription(optionDescription: HTMLDivElement){
-    console.log(optionDescription)
+  toogleDisplayDescription(optionDescription: HTMLDivElement) {
+    console.log(optionDescription);
     optionDescription.classList.toggle('display-cutted');
   }
 
-  changeOption(index:number){
-    if(this.selectedOptions.length>=this.votationObject.maxOptions){
-      this.limitReached=true;
-      if(this.selectedOptions.length>this.votationObject.maxOptions){
+  changeOption(index: number) {
+    if (this.selectedOptions.length >= this.votationObject.maxOptions) {
+      this.limitReached = true;
+      if (this.selectedOptions.length > this.votationObject.maxOptions) {
         this.selectedOptions.pop();
         return;
       }
-    }else if(this.selectedOptions.length<this.votationObject.maxOptions){
-      this.limitReached=false;
+    } else if (this.selectedOptions.length < this.votationObject.maxOptions) {
+      this.limitReached = false;
     }
 
     // console.log(this.selectedOptions)
-    this.votationObject.optionsList[index].selected=!this.votationObject.optionsList[index].selected;
+    this.votationObject.optionsList[index].selected =
+      !this.votationObject.optionsList[index].selected;
   }
 }
