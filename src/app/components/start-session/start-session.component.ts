72a52ba71049be316da9b5d7ae3318ffd69215ac
Adrian González <adrigondo.profesional@gmail.com>
2022-09-18 23:29:38 -0600

    Update: Input styles improved

--- src/app/components/start-session/start-session.component.ts
+++ src/app/components/start-session/start-session.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartSessionComponent implements OnInit {
 
+  remindMe:boolean=false;
+
   constructor() { }
 
   ngOnInit(): void {
